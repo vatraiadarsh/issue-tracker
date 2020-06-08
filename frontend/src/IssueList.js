617ea39e224adfb70c5c431e046381d9368ce806
Adarsha Bhattarai <33037244+vatraiadarsh@users.noreply.github.com>
2020-06-08 22:19:49 +1000

    setting up the state

--- frontend/src/IssueList.js
+++ frontend/src/IssueList.js
@@ -77,13 +77,37 @@ const issues = [
 ];
 
 class IssueList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      issues: issues,
+    };
+    setTimeout(this.createTestIssue.bind(this), 2000);
+  }
+
+  createIssue(newIssue) {
+    const newIssues = this.state.issues.slice();
+    newIssue.id = this.state.issues.length + 1;
+    newIssues.push(newIssue);
+    this.setState({ issues: newIssues });
+  }
+
+  createTestIssue() {
+    this.createIssue({
+      status: "New",
+      owner: "Suraj",
+      created: new Date(),
+      title: "Completion date should be optional",
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Issue Tracker</h1>
         <IssueFilter />
         <hr />
-        <IssueTable issues={issues} />
+        <IssueTable issues={this.state.issues} />
         <hr />
         <IssueAdd />
       </div>
